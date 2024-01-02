cfdf2cf7c0e4f35c0ffd0b5461ec35ff981b85d3
zen-ku <kariyakariya399@gmail.com>
2024-01-03 00:33:36 +0100

    todos loading state created

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TodoList from '../components/TodoList';
 import AddTodoForm from '../components/AddTodoForm';
 import { Todo } from '../types'; // Define Todo type in types.ts
+import { readTodos } from '@/crud';
 
 const Home: React.FC = () => {
+  
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [todosLoaded, setTodosLoaded] = useState(false);
+  
+  useEffect(() => {
+    setTimeout(() => {
+      const localStoragetTodos = readTodos();
+      setTodos(localStoragetTodos);
+      setTodosLoaded(true);
+    }, 1000)
+  }, []);
 
+  
   const addTodo = (title: string) => {
     const newTodo: Todo = {
       id: Math.ceil(Math.random() * 100000).toString(),
@@ -23,8 +35,13 @@ const Home: React.FC = () => {
   return (
     <div>
       <h1>Advanced Todo App</h1> 
-      <AddTodoForm addTodo={addTodo} />
-      <TodoList todos={todos} />
+      { ! todosLoaded ?  "Not loaded" : (
+        <>
+          <AddTodoForm addTodo={addTodo} />
+          <TodoList todos={todos} />
+        </>
+      ) }
+      
     </div>
   );
 };
