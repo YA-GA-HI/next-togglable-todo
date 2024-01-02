f9721314c36a4c564b4b74ef2f58cc65e3b63c4f
zen-ku <kariyakariya399@gmail.com>
2024-01-02 23:47:30 +0100

    todolist form component created

---
+++ components/AddTodoForm.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+interface AddTodoFormProps {
+  addTodo: (title: string) => void;
+}
+
+const AddTodoForm: React.FC<AddTodoFormProps> = ({ addTodo }) => {
+  const [title, setTitle] = useState('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    addTodo(title);
+    setTitle('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Add a new todo..."
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+};
+
+export default AddTodoForm;
