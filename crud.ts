836d22e6dfb7f4659a8ee1ea30a66d80d103ae39
zen-ku <kariyakariya399@gmail.com>
2024-01-03 00:29:11 +0100

    crud has required functions

---
+++ crud.ts
@@ -0,0 +1,41 @@
+import { Todo } from "./types";
+
+
+export const createTodo = (title: string) => {
+    const newTodo: Todo = {
+    id: Math.ceil(Math.random() * 100000).toString(),
+    title: title,
+    createdAt: new Date(),
+    completedAt: null,
+    };
+    const todos = readTodos();
+    todos.push(newTodo)
+    localStorage.setItem("todos", JSON.stringify(todos));
+    return newTodo;
+};
+
+
+
+export const readTodos = () => {
+    const localeStorageData = localStorage.getItem("todos");
+    let todos = localeStorageData ? JSON.parse(localeStorageData) : [];
+    return todos;
+};
+
+
+
+
+export const deleteTodos = ( id: string ) => {
+    const todos = readTodos();
+    for(let i =0; i<todos.length; i++)
+    {
+        const localTodo = todos[i];
+        if(localTodo.id == id)
+        {
+            todos[i].completedAt = new Date();
+        }
+    }
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+    return true;
+};
